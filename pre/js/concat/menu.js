dd1c81f0fe316157d163a12c0434f318f9e31234
Maciek Pastuszka <maciek.p@netkata.com>
2016-09-22 16:19:44 +0200

    scroll timeout

--- pre/js/concat/menu.js
+++ pre/js/concat/menu.js
@@ -2,8 +2,7 @@ define('menu', function () {
     //menu on scroll
     window.addEventListener("scroll", function () {
         var main_nav = document.querySelector(".main-nav"),
-            scroll = (document.documentElement && document.documentElement.scrollTop) ||
-            document.body.scrollTop;
+            scroll = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
         //if scroll is more than 20 change navigation bar
         if (scroll >= 20) {
             main_nav.classList.add("is-move");
@@ -14,7 +13,7 @@ define('menu', function () {
 
     //mobile navigation toggle
     var nav_toggle = document.querySelector(".main-nav__toggle"),
-        nav = document.querySelector(".main-nav__colapse")
+        nav = document.querySelector(".main-nav__colapse"),
     circle = document.querySelector(".main-nav__circle");
 
     //toggle menu on click
@@ -24,3 +23,22 @@ define('menu', function () {
         circle.classList.toggle("is-open");
     });
 });
+
+
+//
+//
+//var scrollTimeout;  // global for any pending scrollTimeout
+//var outerPane = $details.find(".details-pane-outer");
+//$(window).scroll(function () {
+//    if (scrollTimeout) {
+//        // clear the timeout, if one is pending
+//        clearTimeout(scrollTimeout);
+//        scrollTimeout = null;
+//    }
+//    scrollTimeout = setTimeout(scrollHandler, 250);
+//});
+//scrollHandler = function () {
+//    // Check your page position and then
+//    // Load in more results
+//    // outerPane.html();
+//};
