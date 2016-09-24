196d07e8b1f07727f2ee25827166950f94603e13
Maciej <maciej.pastuszka@gmail.com>
2016-09-24 13:14:36 +0200

    menu module, scroll event setinterval

--- pre/js/concat/menu.js
+++ pre/js/concat/menu.js
@@ -1,44 +1,53 @@
 define('menu', function () {
-    //menu on scroll
-    window.addEventListener("scroll", function () {
-        var main_nav = document.querySelector(".main-nav"),
-            scroll = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        //if scroll is more than 20 change navigation bar
+    var main_nav = document.querySelector(".main-nav"),
+        nav_toggle = main_nav.querySelector(".main-nav__toggle"),
+        nav = main_nav.querySelector(".main-nav__colapse"),
+        circle = main_nav.querySelector(".main-nav__circle");
+
+    var menuMove = function () {
+        var scroll = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        /*
+        if scroll is more than 20 change navigation bar
+        */
         if (scroll >= 20) {
             main_nav.classList.add("is-move");
         } else {
             main_nav.classList.remove("is-move");
         }
-    });
+    };
 
-    //mobile navigation toggle
-    var nav_toggle = document.querySelector(".main-nav__toggle"),
-        nav = document.querySelector(".main-nav__colapse"),
-    circle = document.querySelector(".main-nav__circle");
-
-    //toggle menu on click
-    nav_toggle.addEventListener("click", function () {
-        this.classList.toggle("is-open");
+    var mobileNavToggle = function () {
+        nav_toggle.classList.toggle("is-open");
         nav.classList.toggle("is-open");
         circle.classList.toggle("is-open");
-    });
+    };
+
+    var scrollEvent = function () {
+        var scrollInit = false;
+        window.addEventListener("scroll", function () {
+            scrollInit = true;
+        });
+        setInterval(function () {
+            if (scrollInit) {
+                scrollInit = false;
+                menuMove();
+            }
+        }, 250);
+    };
+
+    var events = function () {
+        scrollEvent();
+        nav_toggle.addEventListener("click", mobileNavToggle);
+    };
+
+    var init = function () {
+        events();
+    };
+
+    init();
+
+    //API
+    return {
+        init: init
+    }
 });
-
-
-//
-//
-//var scrollTimeout;  // global for any pending scrollTimeout
-//var outerPane = $details.find(".details-pane-outer");
-//$(window).scroll(function () {
-//    if (scrollTimeout) {
-//        // clear the timeout, if one is pending
-//        clearTimeout(scrollTimeout);
-//        scrollTimeout = null;
-//    }
-//    scrollTimeout = setTimeout(scrollHandler, 250);
-//});
-//scrollHandler = function () {
-//    // Check your page position and then
-//    // Load in more results
-//    // outerPane.html();
-//};
