ad324eaa0b4497dde17d2187218c94cf99e5fb34
Maciej <maciej.pastuszka@gmail.com>
2016-09-26 19:31:35 +0200

    vr update

--- pre/js/concat/vr.js
+++ pre/js/concat/vr.js
@@ -1,32 +1,44 @@
 define('vr', ['ajax'], function (ajax) {
-    function show_vr() {
+    var vr_button = document.getElementById('vrversion'),
+        vr__container = document.getElementById('vr__container'),
+        header__container = document.querySelector('.header__container');
+
+    var events = function () {
+        vr_button.addEventListener('click', showVr);
+    };
+
+    var showVr = function () {
         ajax({
-            type: "GET",
-            url: "aframe/portfolio.html",
-            dataType: "text",
-            onError: function (msg) {
-                console.warn(msg);
-                /* wynik.innerHTML = "<div class=\"alert warning\">Coś poszło nie tak.</div>";*/
+            type: 'GET',
+            url: 'aframe/portfolio.html',
+            dataType: 'text',
+            onError: function (data) {
+                console.warn(data);
             },
-            onSuccess: function (msg) {
-                var vr__container = document.getElementById("vr__container"),
-                    header__container = document.querySelector(".header__container");
-
-                header__container.classList.add("is-vr");
-                vr__container.classList.add("is-loading");
-                vr__container.innerHTML = "<div class=\"loader\"><span></span><span></span><span></span></div>";
-
-
-                setTimeout(function () {
-                    vr__container.innerHTML = msg;
-                }, 1000);
-
-                setTimeout(function () {
-                    vr__container.classList.remove("is-loading");
-                }, 2000);
+            onSuccess: function (data) {
+                show(data);
             }
         });
-    }
-    var vr_button = document.getElementById("vrversion");
-    vr_button.addEventListener("click", show_vr);
+
+        function show(data) {
+            header__container.classList.add('is-vr');
+            vr__container.classList.add('is-loading');
+            vr__container.innerHTML = '<div class="loader"><span></span><span></span><span></span></div>';
+
+            setTimeout(function () {
+                vr__container.innerHTML = data;
+            }, 1000);
+
+            //remove loader
+            setTimeout(function () {
+                vr__container.classList.remove('is-loading');
+            }, 2000);
+        }
+    };
+
+    var init = function () {
+        events();
+    };
+
+    init();
 });
