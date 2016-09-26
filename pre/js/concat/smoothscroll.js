e50ef8b4e6471b796e7f8298cedd82efb0fea35e
Maciej <maciej.pastuszka@gmail.com>
2016-09-26 19:17:17 +0200

    smoothscroll update

--- pre/js/concat/smoothscroll.js
+++ pre/js/concat/smoothscroll.js
@@ -1,39 +1,49 @@
 define('smoothscroll', function () {
-    //scroll animation
     var a = document.querySelectorAll('a[href*="#"]');
-    for (i = 0; i < a.length; i++) {
 
-        a[i].addEventListener("click", function (e) {
-            e.preventDefault();
-            var target_name = this.hash.substr(1),
-                m = 0,
-                target = document.getElementById(target_name),
-                scroll_animation = setInterval(animate_scroll, 1);
-
+    var scrollTo = function (link) {
+        var target_name = link.hash.substr(1),
+            m = 0,
+            target = document.getElementById(target_name),
+            scroll_animation = setInterval(animate_scroll, 1),
             actual_position = (document.documentElement && document.documentElement.scrollTop) ||
-                document.body.scrollTop;
+            document.body.scrollTop,
             target_position = target.offsetTop - 100;
 
-            function animate_scroll() {
-                m = m + 0.3;
-                if (actual_position < target_position) {
-                    actual_position = actual_position + (10 + m);
-                    document.documentElement.scrollTop = actual_position;
-                    document.body.scrollTop = actual_position;
-                    if (actual_position > target_position) {
-                        clearInterval(scroll_animation);
-                    }
+        function animate_scroll() {
+            m = m + 0.3;
 
-                } else {
-                    actual_position = actual_position - (10 + m);
-                    document.documentElement.scrollTop = actual_position;
-                    document.body.scrollTop = actual_position;
-                    if (actual_position < target_position) {
-                        clearInterval(scroll_animation);
-                    }
+            if (actual_position < target_position) {
+                actual_position = actual_position + (10 + m);
+                document.documentElement.scrollTop = actual_position;
+                document.body.scrollTop = actual_position;
+                if (actual_position > target_position) {
+                    clearInterval(scroll_animation);
+                }
+            } else {
+                actual_position = actual_position - (10 + m);
+                document.documentElement.scrollTop = actual_position;
+                document.body.scrollTop = actual_position;
+                if (actual_position < target_position) {
+                    clearInterval(scroll_animation);
                 }
             }
-        });
-    }
+        }
+    };
 
+    var events = function () {
+        for (i = 0; i < a.length; i++) {
+            a[i].addEventListener("click", function (e) {
+                e.preventDefault();
+
+                scrollTo(this);
+            });
+        }
+    };
+
+    var init = function () {
+        events();
+    };
+    
+    init();
 });
