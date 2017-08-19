a818e0905fc524e216c0b9362d6f7df6a6cedb61
Maciej <maciej.pastuszka@gmail.com>
2017-08-19 11:08:43 +0200

    main nav refactor

--- resources/js/components/nav.js
+++ resources/js/components/nav.js
@@ -2,6 +2,7 @@ class Navigation {
     constructor(nav) {
         this.nav = nav;
         this.nav_toggle = nav.querySelector('.main-nav__toggle');
+        this.nav_colapse = nav.querySelector('.js-main-nav__colapse');
         this.nav_circle = nav.querySelector('.main-nav__circle');
         this.state = {
             scroll: true
@@ -28,9 +29,10 @@ class Navigation {
     }
 
     mobileNavToggle() {
-        this.nav.classList.toggle('is-open');
         this.nav_toggle.classList.toggle('is-open');
-        this.nav_circle.classList.toggle('is-open');
+        this.nav_colapse.classList.toggle('is-open');
+
+        console.log(this.nav_colapse);
     }
 
     events() {
