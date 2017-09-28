75f9ff4e415f72d67ad8ad89c1ca2e4d8a2fdad9
Maciej <maciej.pastuszka@gmail.com>
2017-09-28 19:50:41 +0200

    add ajax helper

--- resources/js/components/ajax.js
+++ resources/js/components/ajax.js
@@ -1,40 +1,21 @@
-define('ajax', function () {
-    return function (options) {
-        options = {
-            type: options.type || 'POST',
-            url: options.url || '',
-            data: options.data,
-            onComplete: options.onComplete || function () {},
-            onError: options.onError || function () {},
-            onSuccess: options.onSuccess || function () {},
-            dataType: options.dataType || 'text'
-        };
-
-        var xhr = new XMLHttpRequest();
-        xhr.open(options.type, options.url, true);
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4) {
-                if (httpSuccess(xhr)) {
-                    var returnData = (options.dataType == 'xml') ? xhr.responseXML : xhr.responseText;
-                    options.onSuccess(returnData);
-                } else {
-                    options.onError();
-                }
-                options.onComplete();
-                xhr = null;
-            }
-        };
-
+function ajax(url, methodType) {
+    return new Promise(function (resolve, reject) {
+        const xhr = new XMLHttpRequest();
+        xhr.open(methodType, url, true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.send(options.data);
-
-        function httpSuccess(r) {
-            try {
-                return (r.status >= 200 && r.status < 300 || r.status == 304 || navigator.userAgent.indexOf('Safari') >= 0 && typeof r.status == 'undefined')
-            } catch (e) {
-                return false;
+        xhr.send();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    let response = xhr.responseText;
+                    let responseJSON = JSON.parse(response);
+                    resolve(responseJSON);
+                } else {
+                    reject(xhr.status);
+                }
             }
-        }
-    }
-});
+        };
+    });
+}
+
+export default ajax;
