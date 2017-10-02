a645cb1d4e142a778081b9458e8f8ea0b6310e7d
Maciej <maciej.pastuszka@gmail.com>
2017-10-02 22:05:42 +0200

    update layers

--- resources/js/components/layers.js
+++ resources/js/components/layers.js
@@ -3,11 +3,28 @@ class Layers {
         this.layer_container = layer_container;
         this.layers = layer_container.querySelectorAll('.layer');
         this.hero = document.querySelector('.hero');
+        this.inclination_state = {};
     }
 
-    _moveLayer() {
-        this.layers.forEach((layer) => {
+    _moveLayer(layer, layer_inclination) {
+        let layer_id = layer.getAttribute('data-id');
 
+        if (layer_inclination != this.inclination_state[layer_id]) {
+            this.inclination_state[layer_id] = layer_inclination;
+            layer.style.transform = `translateX(${layer_inclination}px)`;
+        }
+    }
+
+    _calcInclination(cursor_position) {
+        let half_page_width = window.innerWidth / 2;
+        let cursor_inclination = cursor_position - half_page_width;
+        let cursor_inclination_percentage = cursor_inclination / half_page_width;
+
+        this.layers.forEach((layer) => {
+            let layer_range = layer.getAttribute('data-range');
+            let layer_width = layer.offsetWidth;
+            let layer_inclination = parseInt(layer_range * cursor_inclination_percentage - layer_width * 0.1);
+            this._moveLayer(layer, layer_inclination);
         });
     }
 
@@ -18,24 +35,18 @@ class Layers {
         this.hero.addEventListener('mousemove', (e) => {
             event = e;
             scrollInit = true;
-            console.log('move e');
         });
+
         setInterval(() => {
             if (scrollInit) {
-                console.log('move');
                 scrollInit = false;
-                this._moveLayer(event);
+                this._calcInclination(event.pageX);
             }
-        }, 50);
-    }
-
-    _events() {
-
+        }, 100);
     }
 
     init() {
         if (this.layer_container) {
-            this._events();
             this._mouseEventDebounce();
         }
     }
