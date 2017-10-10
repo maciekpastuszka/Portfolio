cfe20146d47c29388db1ddabcfa91411abf2e28f
Maciej <maciej.pastuszka@gmail.com>
2017-10-10 19:00:38 +0200

    vr loader

--- resources/js/components/vr.js
+++ resources/js/components/vr.js
@@ -3,38 +3,19 @@ import ajax from './ajax';
 class Vr {
     constructor(vr_container) {
         this.vr_container = vr_container;
+        this.vr_show_btn = document.querySelector('.js-show-vr');
         this.state = {
             vr_loaded: false
         }
     }
 
-    events() {
-        // window.addEventListener('scroll', () => {
-        //     this.state.scroll = true;
-        // });
-        // this.nav_toggle.addEventListener('click', () => {
-        //     this.mobileNavToggle();
-        //
-        // });
-    }
-
-    _loadAframe() {
-        this._addAframeScript()
-            .then(this._triggerDOMContentLoadedEvent)
-            .then(() => ajax('aframe/portfolio.html', 'GET', 'text'))
-            .then((data) => {
-                this.vr_container.innerHTML = data;
-                this.state.vr_loaded = true;
-            });
-    }
-
     _addAframeScript() {
-        return new Promise(function (resolve, reject) {
+        return new Promise((resolve, reject) => {
             const aframe_script = document.createElement('script');
             aframe_script.setAttribute('src', '/aframe/aframe.min.js');
             document.querySelector('body').appendChild(aframe_script);
             aframe_script.addEventListener('load', () => {
-                resolve('test');
+                resolve();
             });
         });
     }
@@ -45,10 +26,46 @@ class Vr {
         window.document.dispatchEvent(DOMContentLoaded_event);
     }
 
+    _toggleVrMode() {
+        console.log('toggle vr mode');
+    }
+
+    _loadAframe() {
+        return new Promise((resolve) => {
+            if (!this.state.vr_loaded) {
+                /**
+                 * Start loading, fetch script and run aframe then show VR mode
+                 */
+
+                // start loader
+                this._addAframeScript()
+                    .then(this._triggerDOMContentLoadedEvent)
+                    .then(() => ajax('aframe/portfolio.html', 'GET', 'text'))
+                    .then((data) => {
+                        this.vr_container.innerHTML = data;
+                        this.state.vr_loaded = true;
+                        //end loader
+
+                        resolve('script loaded');
+
+                    });
+            } else {
+                /**
+                 * Just show VR mode
+                 */
+                resolve('show vr mode');
+            }
+        });
+    }
+
+    _events() {
+        this.vr_show_btn.addEventListener('click', () => {
+            this._loadAframe().then(this._toggleVrMode);
+        });
+    }
+
     init() {
-        // this.events();
-
-
+        this._events();
     }
 }
 
