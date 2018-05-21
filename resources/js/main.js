70e8629cc4c8118dc0796a163f6d70d9b27d2184
Maciek Pastuszka <maciej.pastuszka@gmail.com>
2018-05-21 14:09:30 +0200

    disable smoothscroll

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,8 +6,8 @@ import Vr from './components/vr';
 const mainNAv = new MainNav(document.querySelector('.main-nav'));
 mainNAv.init();
 
-const smoothScroll = new SmoothScroll();
-smoothScroll.init();
+// const smoothScroll = new SmoothScroll(5);
+// smoothScroll.init();
 
 const layers = new Layers(document.querySelector('.hero__layers'));
 layers.init();
